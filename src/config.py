a06313800c372ee7fa2f106e8b901ea6177135ed
HamanoTaiki <hamano0813@gmail.com>
2025-10-28 15:34:36 +0800

    重构配置项，添加视图像素和文件夹路径配置

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
 import os
 
-from qfluentwidgets import OptionsConfigItem, OptionsValidator, QConfig, qconfig
+from qfluentwidgets import (
+    ConfigItem,
+    FolderValidator,
+    OptionsConfigItem,
+    OptionsValidator,
+    QConfig,
+    RangeConfigItem,
+    RangeValidator,
+    qconfig,
+)
 
 current_path = os.path.dirname(os.path.dirname(__file__)).replace("\\", "/")
 config_path = os.path.join(current_path, "config.json")
@@ -8,6 +17,20 @@ config_path = os.path.join(current_path, "config.json")
 
 class Option(QConfig):
     dpi = OptionsConfigItem("QFluentWidgets", "DPI", 1, OptionsValidator([1, 1.25, 1.5, 1.75, 2]), restart=True)
+
+    view_pixel = RangeConfigItem("View", "Pixel", 12, RangeValidator(12, 144))
+
+    folder_dumps = ConfigItem("Folder", "dumps", "", FolderValidator())
+    folder_backup = ConfigItem("Folder", "backup", "", FolderValidator())
+    folder_export = ConfigItem("Folder", "export", "", FolderValidator())
+
+    font_loader = OptionsConfigItem(
+        "Font",
+        "Loader",
+        [
+            [32, 32, 0, 0, 18, 18],
+        ],
+    )
 
 
 option = Option()
